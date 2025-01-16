d1786695f1ddb4b83bdd655f763c5eea375b76b6
Awes Khan Ayyub Khan <44608315+Awes-Khan@users.noreply.github.com>
2025-01-16 15:50:40 +0530

    Add files via upload

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,58 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React from "react";
+import './../styles/App.css';
+
+
+const App = () => {
+	const [value, setValue] = React.useState("#Testingggg");
+	const [content, setContent] = React.useState("");
+
+	  React.useEffect(() => {
+    const convertedHtml = value
+      .replace(/^# (.*$)/gim, '<h1>$1</h1>') 
+      .replace(/^## (.*$)/gim, '<h2>$1</h2>')
+      .replace(/^### (.*$)/gim, '<h3>$1</h3>')
+      .replace(/\*\*(.*)\*\*/gim, '<strong>$1</strong>') 
+      .replace(/\*(.*)\*/gim, '<em>$1</em>')
+      .replace(/\n/gim, '<br/>')
+      .replace(/\n\n/gim, '<p></p>');
+    	console.log(convertedHtml);
+    setContent(convertedHtml);
+  }, [value]);
+
+
+  return (
+    <div style={{ display: "flex", gap: "20px", padding: "20px" }}>
+
+			<textarea 
+				id="input" 
+				value={value} 
+				placeholder="Type your Markdown here..."
+				style={{
+			          width: "45%",
+			          height: "300px",
+			          padding: "10px",
+			          fontSize: "16px",
+			        }}
+				onChange={(e)=>{
+					setValue(e.target.value);
+				}}
+			></textarea>
+			<div 
+				id="preview"
+		        style={{
+		          width: "45%",
+		          height: "300px",
+		          padding: "10px",
+		          border: "1px solid #ddd",
+		          overflow: "auto",
+		          backgroundColor: "#f9f9f9",
+		        }}
+		        dangerouslySetInnerHTML={{ __html: content }}
+        	>
+			</div>
+    </div>
+  )
+}
+
+export default App
 
